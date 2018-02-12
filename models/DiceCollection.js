c588cb81c23d671b2389f3ddae10c563daa3ccf0
Buwaro <buwaro@gmail.com>
2018-02-12 21:46:50 +0100

    More error handling

--- models/DiceCollection.js
+++ models/DiceCollection.js
@@ -1,4 +1,4 @@
-const diceRegex = /(\d*)d(\d*)/i
+const diceRegex = /(.*)d(.*)/i
 const Dice = require("./dice")
 
 class DiceCollection {
@@ -12,12 +12,26 @@ class DiceCollection {
         for (var diceString of diceStrings) {
             if (diceRegex.test(diceString)) {
                 var [amount, type] = diceRegex.exec(diceString).slice(1,3)
+
+                if (isNaN(amount)) {
+                    throw("The amount of dice is not a number")
+                }
+                else if (amount.length == 0) {
+                    amount = 1
+                }
+                else if (amount < 0) {
+                    throw("You can't roll a negative amount of dice")
+                }
+
+                if (isNaN(type)) {
+                    throw("The amount of sides is not a number")
+                }
+                else if (type <= 1) {
+                    throw("You can't roll a dice with less than 2 sides")
+                }
             }
             else {
-                throw("Can't roll a " + diceString + ",\n Please try something like: 1d6")
-            }
-            if (amount == "") {
-                amount = 1
+                throw("Can't roll a " + diceString + ",\nPlease try something like: 1d6")
             }
 
             var dice = new Dice(type)
