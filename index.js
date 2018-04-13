f914d9517b3d29c2e08204408a2bf4d40ff1bf7c
Buwaro <buwaro@gmail.com>
2018-04-14 01:10:11 +0200

    Making hedron-bot modular

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // Set the project folder location
 global.__basedir = __dirname;
-
+global.__addonsdir = __basedir + "/addons";
 const Telegraf = require('telegraf')
+const fs = require("fs")
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
+// require all addons
+fs.readdirSync(__addonsdir).forEach((file) => {
+  global.__addondir = {}
+  global.__addondir[file] = __addonsdir + "/" + file
+  require(__addondir[file])(bot)
 })
 
 bot.startPolling()
