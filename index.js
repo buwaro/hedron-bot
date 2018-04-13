be70188bef738c8f273a745d65d02008323eb07c
Buwaro <buwaro@gmail.com>
2018-04-14 01:06:56 +0200

    Removing the roll command from hedron
    this is now available as an addon on github.com/buwaro/hedron-roll

--- index.js
+++ index.js
@@ -2,34 +2,9 @@
 global.__basedir = __dirname;
 
 const Telegraf = require('telegraf')
+
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-const Dice = require(__basedir + "/models/Dice")
-const DiceCollection = require(__basedir + "/models/DiceCollection")
-
-bot.command('roll', (ctx) => {
-    try {
-        var params = getParams(ctx)
-        if (params.length == 0 ) {
-            result = new Dice(6).roll()
-        }
-        else {
-        var diceCollection = new DiceCollection()
-        diceCollection.addDices(params)
-        var result = diceCollection.rollDices().join(", ")
-        }
-
-        ctx.reply(result)
-    }
-    catch (error) {
-        ctx.reply(error)
-    }
 })
 
 bot.startPolling()
-
-function getParams(ctx) {
-    var parts = ctx.message.text.split(" ")
-    parts.shift()
-    return parts
-}
